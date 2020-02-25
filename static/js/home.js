c4c246b3aa386a2902d18443884277223ee40206
Marc Garcia <garcia.marc@gmail.com>
2020-02-25 18:22:31 +0000

    Updates to the home page

--- static/js/home.js
+++ static/js/home.js
@@ -1,21 +1,6 @@
 $(document).ready(function() {
   'use strict';
 
-  $(window).load(function() {
-    if (window.innerWidth > 960) {
-      $("#header").css("background-image", 'url("/static/img/home-bg.jpg")');
-    } else {
-      $("#header").css("background-image", 'none');
-    }
-  });
-  $(window).resize(function() {
-    if (window.innerWidth > 960) {
-      $("#header").css("background-image", 'url("/static/img/home-bg.jpg")');
-    } else {
-      $("#header").css("background-image", 'none');
-    }
-  });
-
   var typed = $(".typed");
   $(function() {
     typed.typed({
